4b3723832fc16df481ee6e645c69b0069086cea7
VideoDanilka <lariondanil@gmail.com>
2023-04-30 23:12:01 +0300

    students

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ import sqlite3
 
 app = Flask(__name__)
 
+# Использовать глобальные переменные плохая идея. Следовало сделать класс, и все делать внутри класса.
 pointers = []
 context = {}
 filename = ''
+db_name = ''
 pointer_sample = []
 results = []
+students_names = []
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -21,6 +24,7 @@ def main():
         return render_template('form1.html', file_names=file_names)
     elif request.method == 'POST':
         global filename
+        global db_name
         filename = request.form['file_name']
         db_name = request.form['db_name']
         doc = docx.Document(f'tpl/{filename}')
@@ -32,16 +36,31 @@ def main():
                     pointers.append(a)
         con = sqlite3.connect(f"{db_name}")
         cur = con.cursor()
-        result = cur.execute("""SELECT * FROM fields""").fetchall()
-        for elem in result:
-            f = {}
-            f[elem[1]] = elem[2]
-            pointer_sample.append(f)
-        con.close()
-        return save()
+        students = cur.execute("""SELECT name FROM students""").fetchall()
+        for i in students:
+            for name in i:
+                students_names.append(name)
+        print(students_names)
+        return select_student()
 
 
-def save():
+def select_student():
+    return render_template('form3.html', students_names=students_names)
+
+
+@app.route('/render', methods=['GET', 'POST'])
+def render():
+    student_name = request.form['student_name']
+    print(student_name)
+    con = sqlite3.connect(f"{db_name}")
+    cur = con.cursor()
+    result = cur.execute(f"""SELECT * FROM {student_name}""").fetchall()
+    for elem in result:
+        f = {}
+        f[elem[1]] = elem[2]
+        pointer_sample.append(f)
+    con.close()
+
     return render_template('form2.html', pointers=pointers, pointer_sample=pointer_sample)
 
 
